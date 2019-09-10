0b1020e84ec783ffa4f053ac1a70a469e9ba93b3
Adithya12121992 <aditya.aditya.krishna@gmail.com>
2019-09-09 23:49:15 -0400

    Final Code

--- src/glossaryApp/displayAll.java
+++ src/glossaryApp/displayAll.java
@@ -17,33 +17,13 @@ import java.util.Set;
 
 public class displayAll extends HttpServlet{
 	public void service(HttpServletRequest req,HttpServletResponse res ) throws IOException
-	{
-		
-	/*	HashMap<String, String> hmap = new HashMap<String, String>();
-        hmap.put("apple", "A fruit");
-        hmap.put("company", "A entity that engages in business");
-        hmap.put("business", "Business is the activity of making one's living or making money by producing or buying and selling products");
-        hmap.put("products", "an article or substance that is manufactured or refined for sale");
-        
-        Set set = hmap.entrySet();
-        Iterator iterator = set.iterator();
-        String final_json = "{\"glossary\":[";
-        while (iterator.hasNext()) {
-            Map.Entry mentry = (Map.Entry) iterator.next();
-            String intermediate_json;
-            final_json+="{\"word\":\""+mentry.getKey()+"\",\"meaning\":\""+mentry.getValue()+"\"},";
-        }
-        final_json = final_json.substring(0, final_json.length() - 1);
-        final_json += "]}";
-
-		String k = "This is from onload";*/
-		
-		
-		
+	{		
 		File filehandler = new File("/Users/akm/eclipse-workspace/glossary_java/WebContent/json/final_word_list.txt");
-		//File filehandler = new File("./final_word_list.txt");
+		
 		BufferedReader br = new BufferedReader(new FileReader(filehandler));
-		String st; 
+		String st;
+		
+		// Code for the Java Hashmap, this will have the values for the key and value , ie : The word and its meaning. 
 		LinkedHashMap<String, String> hmap = new LinkedHashMap<String, String>();
 		LinkedHashMap<String, String> hmap_sorted = new LinkedHashMap<String, String>();
 		
@@ -58,11 +38,12 @@ public class displayAll extends HttpServlet{
 		Set set = hmap_sorted.entrySet();
 	    Iterator iterator = set.iterator();
 	    String final_json = "{\"glossary\":[";
+	    
 	    while (iterator.hasNext()) {
             Map.Entry mentry = (Map.Entry) iterator.next();
             final_json+="{\"word\":\""+mentry.getKey()+"\",\"meaning\":\""+mentry.getValue()+"\"},";
-            //System.out.println(mentry.getKey()+" : "+mentry.getValue());
 	    }
+	    
 	    final_json = final_json.substring(0, final_json.length() - 1);
         final_json += "]}";
 		
